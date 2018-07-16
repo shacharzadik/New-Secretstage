b63575b10b2ef19db67987ba60165ba7a9bf0bdd
שחר <shachar@shrs-MacBook-Air.local>
2018-07-16 14:00:38 +0300

    new components - artist-item & event-item

---
+++ src/app/artist-item/artist-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-item',
+  templateUrl: './artist-item.component.html',
+  styleUrls: ['./artist-item.component.css']
+})
+export class ArtistItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
