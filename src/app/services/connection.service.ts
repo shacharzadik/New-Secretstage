48b9188e64384cf5f40f4e344e8a51aa9ddae065
Iyad Saba <Iyad.Saba@clicktale.com>
2018-07-28 12:51:42 +0300

    deleted duplicate files

--- src/app/services/connection.service.ts
+++ src/app/services/connection.service.ts
@@ -1,59 +1,73 @@
-
-import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import {
+  Http,
+  Response,
+  Headers,
+  RequestOptions,
+  ResponseContentType
+} from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Injectable()
-
+ /////////////////////////////////////////////////////////////////
 export class ConnectionService {
+  private HTTP: HttpClient;
+  private header: Headers = new Headers({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  });
+  private authHeader: Headers = new Headers({
+    Authorization: 'Bearer'
+  });
+  private options: Object = new RequestOptions({ headers: this.header });
+  private authOptions: Object = new RequestOptions({
+    headers: this.authHeader
+  });
+  private headerForDownloadFile: Headers = new Headers({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Expose-Headers': 'Content'
+  });
+  private optionsForDownloadFile: Object = new RequestOptions({
+    responseType: ResponseContentType.Blob,
+    headers: this.headerForDownloadFile
+  });
+  private headerForUploadFile: Headers = new Headers({
+    enctype: 'multipart/form-data',
+    'Access-Control-Allow-Origin': '*'
+  });
+  private optionsForUploadFile = new RequestOptions({
+    headers: this.headerForUploadFile
+  });
+  // constructor(private _http: Http, private STORAGE: WebStorageService) {
+  //   this.HTTP = _http;
+  // }
+  constructor(private _http: HttpClient) {
+    this.HTTP = _http;
+  }
 
-    private HTTP: Http
-    private header: Headers = new Headers({
-
-    })
-
-
-    getURL(URLDescription: string) {
-        let ServerAddress: string = ' http://localhost/secretstage';
-        // let LoginAddress : string 
-        // let ArtistAddress : string 
-        // let HostAddress : string 
-        // let EventsAddress : string 
-        // let LoginAddress : string 
-        // let LoginAddress : string 
-
-        // switch(URLDescription) {
-        //     case 'Artist' : {
-        //         return ArtistAddress + 'Artist';
-        //     }
-        // }
-        //     case 'Artist' : {
-        //         return ArtistAddress + 'Artist';
-        //     }
-        // }
-        //     case 'Artist' : {
-        //         return ArtistAddress + 'Artist';
-        //     }
-        // }
-        //     case 'Artist' : {
-        //         return ArtistAddress + 'Artist';
-        //     }
-        // }
-        //     case 'Artist' : {
-        //         return ArtistAddress + 'Artist';
-        //     }
-        // }
-
+  getURL(URLDescription: string): string {
+    const ServerAddress = 'https://localhost//';
+    const LoginAddress: string = ServerAddress + 'Authentication/';
+    switch (URLDescription) {
+      case 'example': {
+        return LoginAddress + 'example';
+      }
     }
+  }
 
-    //~~ Example For Calls
+ /////////////////////////////////////////////////////////////////
+  //  VerifyLogin(objects){
+  //   const ItemSendtoServer = {
+  //     objects
+  //   };
+  //   return this.HTTP.post(
+  //     this.getURL('example'),
+  //     UserToSendToServer,
+  //     this.options
+  //   ).map((response: Response) => response.json());
+  // }
+  //
 
-    // Login(o_User: object) {
-    //     let ItemSentToServer = o_User
-    //     return this.HTTP.post(
-    //         this.getURL('Artist'),
-    //         ItemSendToServer,
-    //         this.Headers
-    //     ).map((Response: Response) => Response.json());
-    // }
 }
